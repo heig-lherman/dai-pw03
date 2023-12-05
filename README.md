87e37799bd8da077686faf7b4186926feef77159
Loïc Herman <loic@loicherman.ch>
2023-12-05 14:43:01 +0100

    chore: main class and docker image auto-build
    closes #1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Practical Work 3 - Distributed System Monitoring
+
+## Concept summary
+
+```mermaid
+flowchart LR
+    P1C[PC1:CPU] --> C
+    P2C[PC2:CPU] --> C
+    P2M[PC2:RAM] --> C
+    P3D[PC3:DSK] --> C
+    C{Aggregator}
+    C <--> R1[Reader 1]
+    C <--> R2[Reader N]
+    style C stroke:orange 
+```
+
+Any PC can send data to the aggregator in a fire-and-forget manner.
+
+
+Readers can then access the aggregated results from the different system producers using
+a simple messaging protocol able to filter 
---
+++ src/main/java/heig/dai/pw03/MonitoringCli.java
@@ -0,0 +1,77 @@
+package heig.dai.pw03;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.HelpCommand;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParseResult;
+import picocli.CommandLine.ScopeType;
+
+/**
+ * Main CLI for the Chess client and server.
+ *
+ * @author Loïc Herman
+ * @author Massimo Steffani
+ */
+@Command(
+        name = "DistributedMonitoring",
+        description = "Aggregate monitoring data from multiple sources.",
+        version = "DistributedMonitoring 1.0.0",
+        mixinStandardHelpOptions = true,
+        subcommands = {
+                HelpCommand.class,
+        }
+)
+public class MonitoringCli implements Runnable {
+
+    boolean[] verbosity = new boolean[0];
+
+    @Option(
+            names = "-v",
+            description = "Change log verbosity. Use -vvv for maximum verbosity.",
+            scope = ScopeType.INHERIT
+    )
+    public void setVerbosity(boolean[] verbosity) {
+        this.verbosity = verbosity;
+    }
+
+    /**
+     * Print basic usage information when the user doesn't use
+     * one of the built-in subcommands.
+     */
+    @Override
+    public void run() {
+        CommandLine.usage(this, System.out);
+    }
+
+    /**
+     * Proxy the default execution strategy, after setting the logger level correctly.
+     */
+    private int executionStrategy(ParseResult parseResult) {
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(
+                switch (verbosity.length) {
+                    case 0 -> Level.WARN;
+                    case 1 -> Level.INFO;
+                    case 2 -> Level.DEBUG;
+                    default -> Level.TRACE;
+                }
+        );
+        return new CommandLine.RunLast().execute(parseResult); // default execution strategy
+    }
+
+    /**
+     * Main entry point for the CLI.
+     *
+     * @param args arguments that will be parsed by picocli
+     */
+    public static void main(String[] args) {
+        MonitoringCli cli = new MonitoringCli();
+        new CommandLine(cli)
+                .setExecutionStrategy(cli::executionStrategy)
+                .execute(args);
+    }
+}
