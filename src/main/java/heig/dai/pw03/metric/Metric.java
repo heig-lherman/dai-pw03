8c73801cff4ea8aec17c4006a351ea264b43629d
Massimo Stefani <massimo.cruz25@gmail.com>
2023-12-08 21:52:19 +0100

    metric add value

--- src/main/java/heig/dai/pw03/metric/Metric.java
+++ src/main/java/heig/dai/pw03/metric/Metric.java
@@ -3,6 +3,8 @@ package heig.dai.pw03.metric;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+
+import heig.dai.pw03.monitor.Monitor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +15,15 @@ public enum Metric {
     DSK("230.0.0.3");
 
     private final String groupAddr;
+
+    public double value() {
+        return switch (this) {
+            case CPU -> Monitor.getCPU();
+            case RAM -> Monitor.getRAM();
+            case DSK -> Monitor.getDSK();
+            default -> throw new IllegalStateException("Unexpected value: " + this);
+        };
+    }
 
     public InetAddress getGroupAddress() {
         try {
