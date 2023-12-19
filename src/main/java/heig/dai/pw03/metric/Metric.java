86ab9c827726b6856a53a5f7d3718a5bef415bea
massteff <42839432+massteff@users.noreply.github.com>
2023-12-19 08:40:46 +0100

    Update Metric.java

--- src/main/java/heig/dai/pw03/metric/Metric.java
+++ src/main/java/heig/dai/pw03/metric/Metric.java
@@ -8,6 +8,9 @@ import heig.dai.pw03.monitor.Monitor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Represents a metric. Each metric has a multicast group address.
+ */
 @RequiredArgsConstructor
 public enum Metric {
     CPU("230.0.0.1"),
@@ -16,6 +19,10 @@ public enum Metric {
 
     private final String groupAddr;
 
+    /**
+     * Get the multicast socket address for this metric.
+     * @return The multicast socket address
+     */
     public InetAddress getGroupAddress() {
         try {
             return InetAddress.getByName(groupAddr);
